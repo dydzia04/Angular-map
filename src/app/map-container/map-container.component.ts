c6797571a286e5ab3c8e0860d1852653256e4b1c
Patryk Dynda <PDyndal@gmail.com>
2020-11-08 00:12:16 +0100

    Added OSM map and layers. Trying to get height okay.

--- src/app/map-container/map-container.component.ts
+++ src/app/map-container/map-container.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
 
 @Component({
   selector: 'app-map-container',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-container.component.sass']
 })
 export class MapContainerComponent implements OnInit {
+  private map: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.map = new Map({
+      view: new View({
+        center: [50.285239, 22.7645465],
+        zoom: 1
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      controls: [],
+      target: 'map'
+    });
   }
 
 }
